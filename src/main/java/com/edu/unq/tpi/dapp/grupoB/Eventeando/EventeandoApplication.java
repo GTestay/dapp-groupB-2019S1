6a9ed3a66b65e58fbb1ce8274edbec5e225c9baa
gaston testay <gaston.testay@gmail.com>
2019-04-09 09:50:23 -0300

    setup de Java, Maven y Travis

---
+++ src/main/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/EventeandoApplication.java
@@ -0,0 +1,13 @@
+package com.edu.unq.tpi.dapp.grupoB.Eventeando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventeandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventeandoApplication.class, args);
+	}
+
+}
