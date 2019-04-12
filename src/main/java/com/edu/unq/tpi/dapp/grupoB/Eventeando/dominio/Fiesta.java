03ed4f6d748923d3090e5b4d2e01cf34acf89520
gaston testay <gaston.testay@gmail.com>
2019-04-12 19:25:12 -0300

    agregada validación para que no se pueda crear una fiesta sin invitados. Quizás cambie a futuro

--- src/main/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/dominio/Fiesta.java
+++ src/main/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/dominio/Fiesta.java
@@ -4,11 +4,14 @@ import java.util.HashMap;
 
 public class Fiesta {
 
+    public static final String ERROR_NO_SE_PUEDE_CREAR_SIN_INVITADOS = "ERROR_NO_SE_PUEDE_CREAR_SIN_INVITADOS";
     private String organizador;
     private HashMap<String, String> invitados;
 
     public Fiesta(String pepito, HashMap<String, String> invitados) {
-
+        if (invitados.isEmpty()) {
+            throw new RuntimeException(ERROR_NO_SE_PUEDE_CREAR_SIN_INVITADOS);
+        }
         organizador = pepito;
         this.invitados = invitados;
     }
