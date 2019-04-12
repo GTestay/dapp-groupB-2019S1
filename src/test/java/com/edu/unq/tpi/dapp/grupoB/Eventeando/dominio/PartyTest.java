f947dedc3133098cb988cf20e538888b617d6385
gaston testay <gaston.testay@gmail.com>
2019-04-12 19:25:12 -0300

    agregada validación para que no se pueda crear una fiesta sin invitados. Quizás cambie a futuro

--- src/test/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/dominio/PartyTest.java
+++ src/test/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/dominio/PartyTest.java
@@ -1,5 +1,6 @@
 package com.edu.unq.tpi.dapp.grupoB.Eventeando.dominio;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -9,16 +10,32 @@ import static org.junit.Assert.assertFalse;
 
 public class PartyTest {
 
+    private String organizador;
+
+    @Before
+    public void setUp() throws Exception {
+        organizador = "Pepito";
+    }
+
     @Test
     public void testSePuedeCrearUnaFiestaEstaTieneOrganizadorYUnaListaDeInvitados() {
-        String pepito = "Pepito";
         HashMap<String, String> invitados = new HashMap<>();
         invitados.put("email@gmail.com", "Juan");
 
-        Fiesta fiestaDePepito = new Fiesta(pepito, invitados);
+        Fiesta fiestaDePepito = new Fiesta(organizador, invitados);
 
-        assertEquals(pepito, fiestaDePepito.organizador());
+        assertEquals(organizador, fiestaDePepito.organizador());
         assertFalse(fiestaDePepito.invitados().isEmpty());
+    }
+
+    @Test
+    public void testNoSePuedeCrearUnaFiestaSinInvitados() {
+        try {
+            new Fiesta(organizador, new HashMap<>());
+        } catch (RuntimeException e) {
+            assertEquals(e.getMessage(), Fiesta.ERROR_NO_SE_PUEDE_CREAR_SIN_INVITADOS);
+        }
+
     }
 
 
