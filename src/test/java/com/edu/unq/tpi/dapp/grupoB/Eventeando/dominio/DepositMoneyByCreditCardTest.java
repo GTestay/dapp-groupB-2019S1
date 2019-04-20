3a2f40704ee954a530a21f8b1bef08063c46dff5
Martin Ezequiel Gonzalez <martinegonzalez95@gmail.com>
2019-04-20 14:08:35 -0300

    Agrega el deposito de dinero por medio de tarjeta de credito

---
+++ src/test/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/dominio/DepositMoneyByCreditCardTest.java
@@ -0,0 +1,65 @@
+package com.edu.unq.tpi.dapp.grupoB.Eventeando.dominio;
+
+import com.edu.unq.tpi.dapp.grupoB.Eventeando.exception.DepositMoneyException;
+import com.edu.unq.tpi.dapp.grupoB.Eventeando.factories.UserFactory;
+import com.edu.unq.tpi.dapp.grupoB.Eventeando.validators.DepositMoneyValidator;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static junit.framework.TestCase.fail;
+import static org.junit.Assert.assertEquals;
+
+public class DepositMoneyByCreditCardTest {
+
+    private User user = UserFactory.user();
+    private LocalDate date = LocalDate.now();
+    private double amount = 1000.00;
+
+    @Test
+    public void creationOfANewDepositMoneyByCreditCard() {
+        DepositMoneyByCreditCard depositMoneyByCreditCard = DepositMoneyByCreditCard.create(user, date, amount);
+
+        assertEquals(user, depositMoneyByCreditCard.user());
+        assertEquals(date, depositMoneyByCreditCard.date());
+        assertEquals(amount, depositMoneyByCreditCard.amount(), 0);
+    }
+
+    @Test
+    public void depositMoneyByCreditCardValidations() {
+        userValidations();
+
+        dateValidations();
+
+        amoutValidations();
+    }
+
+    private void amoutValidations() {
+        try {
+            DepositMoneyByCreditCard.create(user, date, -200.00);
+            fail();
+        } catch (DepositMoneyException error) {
+            assertEquals(error.getMessage(), DepositMoneyValidator.AMOUNT_HAS_NEGATIVE_VALUE);
+        }
+    }
+
+    private void dateValidations() {
+        try {
+            DepositMoneyByCreditCard.create(user, null, amount);
+            fail();
+        } catch (DepositMoneyException error) {
+            assertEquals(error.getMessage(), DepositMoneyValidator.DEPOSIT_MONEY_IS_INVALID_WITHOUT_DATE);
+        }
+    }
+
+    private void userValidations() {
+        try {
+            DepositMoneyByCreditCard.create(null, date, amount);
+            fail();
+        } catch (DepositMoneyException error) {
+            assertEquals(error.getMessage(), DepositMoneyValidator.DEPOSIT_MONEY_IS_INVALID_WITHOUT_USER);
+        }
+    }
+
+}
+
