29d4d5de32f83fa10bd796baea9b5d7ef4f9e570
Martin Ezequiel Gonzalez <martinegonzalez95@gmail.com>
2019-04-20 14:25:22 -0300

    Crea depositos de dinero genericos

--- src/main/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/dominio/DepositMoneyByCash.java
+++ src/main/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/dominio/DepositMoneyByCash.java
@@ -1,34 +1,13 @@
 package com.edu.unq.tpi.dapp.grupoB.Eventeando.dominio;
 
-import com.edu.unq.tpi.dapp.grupoB.Eventeando.validators.DepositMoneyValidator;
-
 import java.time.LocalDate;
 
-public class DepositMoneyByCash {
-    private User user;
-    private LocalDate date;
-    private double amount;
-
+public class DepositMoneyByCash extends DepositMoney {
     public static DepositMoneyByCash create(User user, LocalDate date, double amount) {
         DepositMoneyByCash instance = new DepositMoneyByCash();
-        DepositMoneyValidator validator = new DepositMoneyValidator();
 
-        instance.user = validator.validateUser(user);
-        instance.date = validator.validateDate(date);
-        instance.amount = validator.validateAmount(amount);
+        madeValidations(user, date, amount, instance);
 
         return instance;
-    }
-
-    public User user() {
-        return user;
-    }
-
-    public LocalDate date() {
-        return date;
-    }
-
-    public double amount() {
-        return amount;
     }
 }
