dd87fd1894a5d115a01837a4bbd1cb5e3975ec2f
Martin Ezequiel Gonzalez <martinegonzalez95@gmail.com>
2019-04-21 23:51:52 -0300

    Crea los test de transaccion monetario que son generico para los 3 tipos

--- src/test/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/dominio/DepositByCashTest.java
+++ src/test/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/dominio/DepositByCashTest.java
@@ -1,19 +1,10 @@
 package com.edu.unq.tpi.dapp.grupoB.Eventeando.dominio;
 
-import com.edu.unq.tpi.dapp.grupoB.Eventeando.exceptions.MoneyTransactionException;
-import com.edu.unq.tpi.dapp.grupoB.Eventeando.factories.UserFactory;
-import com.edu.unq.tpi.dapp.grupoB.Eventeando.validators.MoneyTransactionValidator;
 import org.junit.Test;
-import java.time.LocalDate;
 
-import static junit.framework.TestCase.fail;
 import static org.junit.Assert.assertEquals;
 
-public class DepositByCashTest {
-
-    private User user = UserFactory.user();
-    private LocalDate date = LocalDate.now();
-    private double amount = 1000.00;
+public class DepositByCashTest extends MoneyTransactionTest {
 
     @Test
     public void creationOfANewDepositMoneyByCash() {
@@ -32,33 +23,5 @@ public class DepositByCashTest {
 
         amoutValidations();
     }
-
-    private void amoutValidations() {
-        try {
-            DepositByCash.create(user, date, -200.00);
-            fail();
-        } catch (MoneyTransactionException error) {
-            assertEquals(error.getMessage(), MoneyTransactionValidator.AMOUNT_HAS_NEGATIVE_VALUE);
-        }
-    }
-
-    private void dateValidations() {
-        try {
-            DepositByCash.create(user, null, amount);
-            fail();
-        } catch (MoneyTransactionException error) {
-            assertEquals(error.getMessage(), MoneyTransactionValidator.MONEY_TRANSACTION_IS_INVALID_WITHOUT_DATE);
-        }
-    }
-
-    private void userValidations() {
-        try {
-            DepositByCash.create(null, date, amount);
-            fail();
-        } catch (MoneyTransactionException error) {
-            assertEquals(error.getMessage(), MoneyTransactionValidator.MONEY_TRANSACTION_IS_INVALID_WITHOUT_USER);
-        }
-    }
-
 }
 
