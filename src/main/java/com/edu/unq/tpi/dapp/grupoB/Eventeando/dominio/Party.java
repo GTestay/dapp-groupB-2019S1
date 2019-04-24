9584c4c7dd42aad17c9e7cb6c14f1b6206913ba5
gaston testay <gaston.testay@gmail.com>
2019-04-24 20:52:57 -0300

    La Party(Fiesta) ahora es un evento.

--- src/main/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/dominio/Party.java
+++ src/main/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/dominio/Party.java
@@ -1,61 +1,45 @@
 package com.edu.unq.tpi.dapp.grupoB.Eventeando.dominio;
 
+import com.edu.unq.tpi.dapp.grupoB.Eventeando.exceptions.EventException;
+import com.edu.unq.tpi.dapp.grupoB.Eventeando.validators.EventValidator;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.DoubleStream;
 
-public class Party {
+import static com.edu.unq.tpi.dapp.grupoB.Eventeando.validators.EventValidator.ERROR_CAN_NOT_ADD_EXPENSE_WHOSE_PRICE_IS_NEGATIVE;
+import static com.edu.unq.tpi.dapp.grupoB.Eventeando.validators.EventValidator.ERROR_THE_USER_WAS_NOT_INVITED;
 
-    public static final String ERROR_CAN_NOT_BE_CREATED_WITHOUT_GUESTS = "error, can not be created without guests";
-    private static final String ERROR_CAN_NOT_ADD_SUPPLIES_WHOSE_PRICE_IS_NEGATIVE = "error, can't not add a supplies whose price is negative";
-    public static final String ERROR_THE_USER_WAS_NOT_INVITED = "error, the user was not invited";
-    private String organizer;
-    private HashMap<String, String> guests;
+public class Party extends Event {
+
     private LocalDateTime invitationLimitDate;
     private Double pricePerAssistant;
-    private HashMap<String, Double> supplies;
     private List<String> guestConfirmations;
 
-    public Party(String organizer, HashMap<String, String> guests, LocalDateTime invitationLimitDate, Double pricePerAssistant) {
-        this.invitationLimitDate = invitationLimitDate;
-        this.pricePerAssistant = pricePerAssistant;
-        if (guests.isEmpty()) {
-            throw new RuntimeException(ERROR_CAN_NOT_BE_CREATED_WITHOUT_GUESTS);
-        }
-        this.organizer = organizer;
-        this.guests = guests;
-        supplies = new HashMap<>();
-        guestConfirmations = new ArrayList<>();
+    public static Party create(User organizer, List<User> assistants, LocalDateTime invitationLimitDate, Double pricePerAssistant) {
+        EventValidator eventValidator = new EventValidator();
+        Party instance = new Party();
+        instance.invitationLimitDate = invitationLimitDate;
+        instance.pricePerAssistant = pricePerAssistant;
+        instance.organizer = organizer;
+        instance.guests = eventValidator.validateAssistants(assistants);
+        instance.expenses = new HashMap<>();
+        instance.guestConfirmations = new ArrayList<>();
+        return instance;
     }
 
-
-    public String organizer() {
-        return organizer;
-    }
-
-    public HashMap<String, String> guests() {
-        return guests;
-    }
 
     public LocalDateTime invitationLimitDate() {
         return invitationLimitDate;
     }
 
-    public void addSupply(String supplyName, Double supplyPrice) {
+    public void addExpense(String supplyName, Double supplyPrice) {
         if (supplyPrice < 0) {
-            throw new RuntimeException(ERROR_CAN_NOT_ADD_SUPPLIES_WHOSE_PRICE_IS_NEGATIVE);
+            throw new RuntimeException(ERROR_CAN_NOT_ADD_EXPENSE_WHOSE_PRICE_IS_NEGATIVE);
         }
-        supplies.put(supplyName, supplyPrice);
-    }
-
-    public Double totalPriceOfSupplies() {
-        return allSuppliesPrices().sum();
-    }
-
-    private DoubleStream allSuppliesPrices() {
-        return supplies.values().stream().mapToDouble(price -> price);
+        expenses.put(supplyName, supplyPrice);
     }
 
     public void confirmAssistance(String anEmail) {
@@ -63,29 +47,19 @@ public class Party {
         guestConfirmations.add(anEmail);
     }
 
-    private void validateThatTheUserWasInvited(String anEmail) {
-        if (!isInvited(anEmail)) {
-            throw new RuntimeException(ERROR_THE_USER_WAS_NOT_INVITED);
-        }
-    }
-
-    private boolean isInvited(String anEmail) {
-        return this.guests.containsKey(anEmail);
-    }
-
     public Double totalCost() {
-        return suppliesCostPerAssistant() + costPerConfirmedAssistance();
+        return expensesCostPerAssistant() + costPerConfirmedAssistance();
     }
 
-    private double suppliesCostPerAssistant() {
-        return sizeOfConfirmations() * totalPriceOfSupplies();
+    private double expensesCostPerAssistant() {
+        return quantityOfConfirmations() * expensesTotalCost();
     }
 
     private Double costPerConfirmedAssistance() {
-        return pricePerAssistant * this.sizeOfConfirmations();
+        return pricePerAssistant * this.quantityOfConfirmations();
     }
 
-    private Integer sizeOfConfirmations() {
+    private Integer quantityOfConfirmations() {
         return guestConfirmations.size();
     }
 }
