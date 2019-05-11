0dae78334a521d8e4f43f3c055631315e0ca7393
gaston testay <gaston.testay@gmail.com>
2019-05-11 14:27:02 -0300

    Reificacion del concepto de una cuenta Abierta del evento donde depositan dinero los invitados.

--- src/main/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/dominio/BaquitaCrowdFundingEvent.java
+++ src/main/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/dominio/BaquitaCrowdFundingEvent.java
@@ -2,46 +2,31 @@ package com.edu.unq.tpi.dapp.grupoB.Eventeando.dominio;
 
 import com.edu.unq.tpi.dapp.grupoB.Eventeando.validators.EventValidator;
 
-import static com.edu.unq.tpi.dapp.grupoB.Eventeando.validators.EventValidator.ERROR_CAN_NOT_ADD_MORE_FUNDS_THAN_IS_REQUIRED_IN_THE_CROWDFUNDING_EVENT;
-import static com.edu.unq.tpi.dapp.grupoB.Eventeando.validators.EventValidator.ERROR_THE_AMOUNT_IS_INVALID;
-
 public class BaquitaCrowdFundingEvent extends Event {
 
-    private Double funds = 0.0;
+    private final SharedAccount sharedAccount = new SharedAccount();
 
-    public double totalFunds() {
-        return funds;
+    public Double totalMoneyRaised() {
+        return sharedAccount.actualFunds();
     }
 
     public void addFunds(User guest, Double amount) {
         validateThatTheUserWasInvited(guest.email());
         validateThatTheEventIsNotFullyFunded();
-        validateThatTheAmountNotSurpassRequiredFunds(amount);
-        validateThatTheAmountIsValid(amount);
-
-        funds += amount;
-    }
-
-    private void validateThatTheAmountIsValid(Double amount) {
-        if (amount <= 0) {
-            throwEventException(ERROR_THE_AMOUNT_IS_INVALID);
-        }
-    }
-
-    private void validateThatTheAmountNotSurpassRequiredFunds(Double amount) {
-        if (funds + amount > totalCost()) {
-            throwEventException(ERROR_CAN_NOT_ADD_MORE_FUNDS_THAN_IS_REQUIRED_IN_THE_CROWDFUNDING_EVENT);
-        }
-
+        sharedAccount.addFunds(guest, amount, this);
     }
 
     private void validateThatTheEventIsNotFullyFunded() {
-        if (funds.equals(this.totalCost())) {
+        if (isFund()) {
             throwEventException(EventValidator.ERROR_CAN_NOT_ADD_MORE_MONEY_THE_CROWDFUNDING_EVENT_IS_FUNDED);
         }
     }
 
     public boolean isFund() {
-        return funds.equals(this.totalCost());
+        return sharedAccount.actualFunds().equals(this.totalCost());
+    }
+
+    public boolean hasAddedFunds(User user) {
+        return sharedAccount.hasAddedFunds(user);
     }
 }
