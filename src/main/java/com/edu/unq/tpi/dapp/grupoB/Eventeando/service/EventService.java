f0c970da131ad22e05ece2e5df23f802c73da823
gaston testay <gaston.testay@gmail.com>
2019-05-11 11:55:48 -0300

    El service usa el dao para obtener a los eventos.

--- src/main/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/service/EventService.java
+++ src/main/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/service/EventService.java
@@ -1,15 +1,24 @@
 package com.edu.unq.tpi.dapp.grupoB.Eventeando.service;
 
 import com.edu.unq.tpi.dapp.grupoB.Eventeando.dominio.Event;
+import com.edu.unq.tpi.dapp.grupoB.Eventeando.repository.EventDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class EventService {
 
+    private final EventDao eventDao;
+
+    @Autowired
+    public EventService(EventDao eventDao) {
+        this.eventDao = eventDao;
+    }
+
     public List<Event> allEvents() {
-        return new ArrayList<>();
+        return eventDao.findAll();
+
     }
 }
