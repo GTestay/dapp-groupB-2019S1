025e543eec48858539105f6cd0b97196dd532680
gaston testay <gaston.testay@gmail.com>
2019-05-18 16:20:11 -0300

    Agregado cambios para Spring del PR Anterior. -Cambio del tipo de dao usado y como se testea.

--- src/main/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/service/EventService.java
+++ src/main/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/service/EventService.java
@@ -1,26 +1,23 @@
 package com.edu.unq.tpi.dapp.grupoB.Eventeando.service;
 
 import com.edu.unq.tpi.dapp.grupoB.Eventeando.dominio.Event;
+import com.edu.unq.tpi.dapp.grupoB.Eventeando.persistence.EventDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class EventService {
 
-    private final CrudRepository<Event, Long> eventDao;
+    private final EventDao eventDao;
 
     @Autowired
-    public EventService(CrudRepository eventDao) {
+    public EventService(EventDao eventDao) {
         this.eventDao = eventDao;
     }
 
     public List<Event> allEvents() {
-        List<Event> list = new ArrayList<>();
-        eventDao.findAll().forEach(list::add);
-        return list;
+        return eventDao.findAll();
     }
 }
