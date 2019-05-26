503a98960eab8a1863bff832892f9fdd786a67af
gaston testay <gaston.testay@gmail.com>
2019-05-26 19:44:58 -0300

    cambiada la serializacion del usuario, ahora se manda la pass( CAMBIAR )

--- src/test/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/webService/ControllerTest.java
+++ src/test/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/webService/ControllerTest.java
@@ -3,18 +3,22 @@ package com.edu.unq.tpi.dapp.grupoB.Eventeando.webService;
 
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ContextConfiguration
 @AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@Transactional
 public abstract class ControllerTest {
 
     protected MockMvc clientRest;
--- src/test/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/webService/UserControllerTest.java
+++ src/test/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/webService/UserControllerTest.java
@@ -6,17 +6,18 @@ import com.edu.unq.tpi.dapp.grupoB.Eventeando.factories.UserFactory;
 import com.edu.unq.tpi.dapp.grupoB.Eventeando.persistence.UserDao;
 import com.edu.unq.tpi.dapp.grupoB.Eventeando.service.UserService;
 import com.edu.unq.tpi.dapp.grupoB.Eventeando.validators.UserValidator;
-import org.assertj.core.api.Assertions;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -24,33 +25,42 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 public class UserControllerTest extends ControllerTest {
 
-
     @Autowired
     UserDao userDao;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+    private UserFactory userFactory;
 
     @Before
     public void setUp() throws Exception {
         super.setUp();
+        userFactory = new UserFactory();
     }
 
     @Test
     public void anUserIsCreated() throws Exception {
-
-        UserFactory userFactory = new UserFactory();
-
         User user = userFactory.user();
-
         JSONObject bodyRequest = getUserBody(user);
 
-        ResultActions perform = clientRest.perform(post(url()).contentType(MediaType.APPLICATION_JSON).content(bodyRequest.toString()));
-        perform.andExpect(status().isCreated())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+        ResultActions perform = clientRest.perform(post(url())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(bodyRequest.toString()));
+
+        MvcResult result = perform
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andReturn();
+
+        String responseString = result.getResponse().getContentAsString();
+        JSONObject jsonResponse = new JSONObject(responseString);
+        jsonResponse.remove("id");
+        JSONAssert.assertEquals(jsonResponse, bodyRequest, true);
     }
 
     @Test
     public void anUserCanNotBeCreatedBecauseTheRequestIsInvalid() throws Exception {
 
-        UserFactory userFactory = new UserFactory();
 
         User user = userFactory.user();
 
@@ -60,29 +70,26 @@ public class UserControllerTest extends ControllerTest {
 
         MvcResult mvcResult = perform.andExpect(status().isBadRequest())
                 .andReturn();
-        Assertions.assertThat(mvcResult.getResolvedException()).hasMessageContaining(UserValidator.USER_EMAIL_IS_INVALID);
+        assertThat(mvcResult.getResolvedException()).hasMessageContaining(UserValidator.USER_EMAIL_IS_INVALID);
     }
 
 
     @Test
     public void anUserIsRetrieved() throws Exception {
 
-        UserFactory userFactory = new UserFactory();
-
         User userPersisted = userDao.save(userFactory.user());
 
-        JSONObject bodyRequest = getUserBody(userPersisted);
-
         String urlTemplate = url() + "/" + userPersisted.id();
-        ResultActions perform = clientRest.perform(get(urlTemplate).contentType(MediaType.APPLICATION_JSON));
+        ResultActions perform = clientRest
+                .perform(get(urlTemplate).contentType(MediaType.APPLICATION_JSON));
 
         MvcResult mvcResult = perform.andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andReturn();
         String contentAsString = mvcResult.getResponse().getContentAsString();
-        bodyRequest.remove("password");
-        JSONObject actual = new JSONObject(contentAsString);
-        assertEquals(bodyRequest.toString(), actual.toString());
+
+        assertThat(objectMapper.writeValueAsString(userPersisted))
+                .isEqualToIgnoringWhitespace(contentAsString);
 
     }
 
@@ -95,7 +102,8 @@ public class UserControllerTest extends ControllerTest {
         MvcResult mvcResult = perform.andExpect(status().isNotFound())
                 .andReturn();
 
-        Assertions.assertThat(mvcResult.getResolvedException()).hasMessageContaining(UserService.messageUserNotFound());
+        assertThat(mvcResult.getResolvedException())
+                .hasMessageContaining(UserService.messageUserNotFound());
     }
 
     private JSONObject getUserBody(User user) throws JSONException {
