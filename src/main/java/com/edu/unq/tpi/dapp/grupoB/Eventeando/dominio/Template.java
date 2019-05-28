fd473d4475189fef2f73fa5e7d4a2d55c6fc7cee
gaston testay <gaston.testay@gmail.com>
2019-05-27 23:36:49 -0300

    Se pueden crear templates. Estos tienen insumos (expenses) y la descripcion. Los templates generan los 4 tipos de eventos. Al generar un nuevo evento, se guarda al usuario para futuras querys.

---
+++ src/main/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/dominio/Template.java
@@ -0,0 +1,70 @@
+package com.edu.unq.tpi.dapp.grupoB.Eventeando.dominio;
+
+import com.edu.unq.tpi.dapp.grupoB.Eventeando.validators.Validator;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Template {
+
+    public static final String INVALID_TEMPLATE_CREATION = "Invalid Template creation";
+
+    private ArrayList<User> usesForUsers;
+    private String description;
+    private List<Expense> expenses;
+
+    private Template(String description, List<Expense> expenses) {
+        this.usesForUsers = new ArrayList<>();
+        this.description = description;
+        this.expenses = expenses;
+    }
+
+    public static Template create(String description, List<Expense> expenses) {
+        validateTemplateCreation(description, expenses);
+
+        return new Template(description, expenses);
+    }
+
+    private static void validateTemplateCreation(String description, List<Expense> expenses) {
+        Validator validator = new Validator();
+        validator.validateEmptinessOf(description, new RuntimeException(INVALID_TEMPLATE_CREATION));
+        validator.validateEmptinessOf(expenses, new RuntimeException(INVALID_TEMPLATE_CREATION));
+    }
+
+    public String description() {
+        return description;
+    }
+
+    public List<Expense> expenses() {
+        return expenses;
+    }
+
+    public Party generateNewPartyEventWith(User user, List<User> guests, LocalDateTime anInvitationLimitDate) {
+        Party party = Party.create(user, description, guests, expenses, anInvitationLimitDate, 0.0);
+        this.usesForUsers.add(user);
+        return party;
+    }
+
+    public Boolean hasUser(User user) {
+        return this.usesForUsers.stream().anyMatch(organizer -> organizer.hasThisEmail(user.email()));
+    }
+
+    public PotluckEvent generateNewPotluckEventWith(User user, List<User> guests) {
+        PotluckEvent potluckEvent = PotluckEvent.create(user, description, guests, expenses);
+        this.usesForUsers.add(user);
+        return potluckEvent;
+    }
+
+    public BaquitaSharedExpensesEvent generateNewBaquitaSharedExpensesEventWith(User user, List<User> guests) {
+        BaquitaSharedExpensesEvent potluckEvent = BaquitaSharedExpensesEvent.create(user, description, guests, expenses);
+        this.usesForUsers.add(user);
+        return potluckEvent;
+    }
+
+    public BaquitaCrowdFundingEvent generateNewBaquitaCrowdFundingEventWith(User user, List<User> guests) {
+        BaquitaCrowdFundingEvent potluckEvent = BaquitaCrowdFundingEvent.create(user, description, guests, expenses);
+        this.usesForUsers.add(user);
+        return potluckEvent;
+    }
+}
--- src/main/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/validators/Validator.java
+++ src/main/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/validators/Validator.java
@@ -4,14 +4,14 @@ import java.util.List;
 
 public class Validator {
 
-    protected <Type> Type validateNullityOf(Type field, RuntimeException exception) {
+    public <Type> Type validateNullityOf(Type field, RuntimeException exception) {
         if (field == null) {
             throw exception;
         }
         return field;
     }
 
-    protected String validateLenghtBetween(String field, int lowerLimmit, int upperLimmit, RuntimeException exception) {
+    public String validateLenghtBetween(String field, int lowerLimmit, int upperLimmit, RuntimeException exception) {
         int fieldLenght = field.length();
 
         if (fieldLenght < lowerLimmit || fieldLenght > upperLimmit) {
@@ -20,21 +20,21 @@ public class Validator {
         return field;
     }
 
-    protected <T> List<T> validateEmptinessOf(List<T> aList, RuntimeException aRuntimeExceptionToBeThrown) {
+    public <T> List<T> validateEmptinessOf(List<T> aList, RuntimeException aRuntimeExceptionToBeThrown) {
         if (aList == null || aList.isEmpty()) {
             throw aRuntimeExceptionToBeThrown;
         }
         return aList;
     }
 
-    protected String validateEmptinessOf(String aString, RuntimeException aRuntimeExceptionToBeThrown) {
+    public String validateEmptinessOf(String aString, RuntimeException aRuntimeExceptionToBeThrown) {
         if (aString == null || aString.isEmpty()) {
             throw aRuntimeExceptionToBeThrown;
         }
         return aString;
     }
 
-    protected Double validateNegativiyOf(Double aNumber, RuntimeException exception) {
+    public Double validateNegativiyOf(Double aNumber, RuntimeException exception) {
         if (aNumber == null || aNumber < 0) {
             throw exception;
         }
