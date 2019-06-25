53a1d4cc909e23d39d9e16d6895ff30277605c0c
Martin Ezequiel Gonzalez <martinegonzalez95@gmail.com>
2019-06-25 20:41:48 -0300

    agregado logger log4j utilizando aspecto en servicios

---
+++ src/main/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/aspect/LoggerAspect.java
@@ -0,0 +1,27 @@
+package com.edu.unq.tpi.dapp.grupoB.Eventeando.aspect;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class LoggerAspect {
+
+    Logger logger = LogManager.getLogger(LoggerAspect.class);
+
+    @Around("execution(public * com.edu.unq.tpi.dapp.grupoB.Eventeando.service.*.*(..)))")
+    public Object log(ProceedingJoinPoint pjp) throws Throwable {
+        logger.info(pjp.getSignature());
+        Arrays.asList(pjp.getArgs()).forEach(arg -> logger.info(arg));
+        Object proceed = pjp.proceed();
+
+        return proceed;
+    }
+}
