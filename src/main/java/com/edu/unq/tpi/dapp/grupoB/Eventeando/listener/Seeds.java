242c1311989c22136f3fcd010734767a094b1a30
Martin Ezequiel Gonzalez <martinegonzalez95@gmail.com>
2019-07-02 16:36:26 -0300

    agregamos  sobre usuarios a la demo

--- src/main/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/listener/Seeds.java
+++ src/main/java/com/edu/unq/tpi/dapp/grupoB/Eventeando/listener/Seeds.java
@@ -6,6 +6,7 @@ import com.edu.unq.tpi.dapp.grupoB.Eventeando.factory.UserFactory;
 import com.edu.unq.tpi.dapp.grupoB.Eventeando.persistence.EventDao;
 import com.edu.unq.tpi.dapp.grupoB.Eventeando.persistence.ExpenseDao;
 import com.edu.unq.tpi.dapp.grupoB.Eventeando.persistence.UserDao;
+import com.edu.unq.tpi.dapp.grupoB.Eventeando.service.AccountManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -25,12 +26,14 @@ public class Seeds implements ApplicationRunner {
     private ExpenseDao expenseDao;
     private EventDao eventDao;
     private EventFactory eventFactory;
+    private AccountManagerService accountManagerService;
 
     @Autowired
-    public Seeds(UserDao userDao, ExpenseDao expenseDao, EventDao eventDao) {
+    public Seeds(UserDao userDao, ExpenseDao expenseDao, EventDao eventDao, AccountManagerService accountManagerService) {
         this.userDao = userDao;
         this.expenseDao = expenseDao;
         this.eventDao = eventDao;
+        this.accountManagerService = accountManagerService;
         eventFactory = new EventFactory();
         userFactory = new UserFactory();
     }
@@ -46,10 +49,25 @@ public class Seeds implements ApplicationRunner {
         List<Expense> expenses = eventFactory.expenses();
         expenseDao.saveAll(expenses);
 
+        createMartin();
+        createGaston();
+
         seedParties(expenses);
         seedBaquitasShared(expenses);
         seedCrowdFundings(expenses);
         seedPotlucks(expenses);
+    }
+
+    protected void createMartin() {
+        User martin  = User.create("Martin Ezequiel", "Gonzalez", "martinegonzalez95@gmail.com", "P4SSW0RD", userFactory.birthday());
+        userDao.save(martin);
+        martin.cashDeposit(500.00, accountManagerService);
+    }
+
+    protected void createGaston() {
+        User gaston  = User.create("Gaston Ezequiel", "Testay", "gaston.testay@gmail.com", "P4SSW0RD", userFactory.birthday());
+        userDao.save(gaston);
+        gaston.cashDeposit(750.00, accountManagerService);
     }
 
     public void seedPotlucks(List<Expense> expenses) {
