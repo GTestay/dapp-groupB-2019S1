eada0fba4248a86201d1c488851c62d3a3b6b129
gastontestay <gaston.testay@gmail.com>
2019-07-16 17:30:00 -0300

    se ve la puntuacion de los eventos en el front.

--- src/main/client/eventeando-front/src/components/EventView.js
+++ src/main/client/eventeando-front/src/components/EventView.js
@@ -5,18 +5,20 @@ import { EventViewGuests } from './EventViewGuests'
 import { EventViewExpenses } from './EventViewExpenses'
 import { SubEventDescription } from './SubEventDescription'
 import { HighDivider } from './utilComponents/HighDivider'
+import { obtainEventScore } from '../api/eventApi'
 
 class EventView extends Component {
   constructor (props, context) {
     super(props, context)
     this.state = {
-      event: null
+      event: null,
+      eventScore: 0
     }
   }
 
   componentDidMount () {
     const { event, user } = this.props.location.state
-    this.setState({ event, user })
+    this.setState({ event, user },() => this.obtainScore(event))
   }
 
   render () {
@@ -24,6 +26,7 @@ class EventView extends Component {
       <Container>
         <Segment>
           {this.showEventOrganizer()}
+          {this.showEventScore()}
           {this.showEventDescription()}
           {this.showEventExtras()}
           {this.showEventGuests()}
@@ -44,6 +47,15 @@ class EventView extends Component {
       id: 'viewEvent.eventOrganizer'
     })
     return HighDivider(<Icon name="user"/>, `${eventOrganizerTitle} ${name} ${lastname}`, React.Fragment)
+  }
+
+  showEventScore () {
+    const intl = this.props.intl
+    const eventOrganizerTitle = intl.formatMessage({
+      id: 'viewEvent.score',
+      defaultMessage: 'Score'
+    })
+    return HighDivider(<Icon name="star"/>, `${eventOrganizerTitle}: ${this.state.eventScore}`, React.Fragment)
   }
 
   showEventDescription () {
@@ -78,6 +90,12 @@ class EventView extends Component {
   showEventExtras () {
     return <SubEventDescription event={this.getEvent()}/>
   }
+
+  obtainScore = event => {
+    obtainEventScore(event.id).then(eventScore =>
+      this.setState({ eventScore })
+    )
+  };
 }
 
 EventView = injectIntl(EventView)
